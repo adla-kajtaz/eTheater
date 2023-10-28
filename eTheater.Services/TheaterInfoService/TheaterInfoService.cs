78ec30acdfbb0d00cab24d4e055243ac4eeaaaf1
adla-kajtaz <adla.kajtaz@edu.fit.ba>
2023-10-28 19:15:18 +0200

    Override addFilter method for theater info

--- eTheater.Services/TheaterInfoService/TheaterInfoService.cs
+++ eTheater.Services/TheaterInfoService/TheaterInfoService.cs
@@ -18,6 +18,14 @@ namespace eTheater.Services
 
         }
 
+        public override IQueryable<eTheater.Services.Database.TheaterInfo> AddFilter(IQueryable<eTheater.Services.Database.TheaterInfo> query, BaseSearchObject search = null)
+        {
+            var filteredQuery = base.AddFilter(query, search);
+
+            filteredQuery = filteredQuery.Where(x => x.IsDeleted == false);
+            return filteredQuery;
+        }
+
         public override Model.TheaterInfo Delete(int id)
         {
             var entity = _context.TheaterInfos.Find(id);
